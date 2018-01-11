14d11e08be57f40169048f70a00c329b9907b590
Bárbara Casac <barbaracasaclj@hotmail.com>
2018-01-11 16:38:46 -0200

    Alteração no menu de opções

--- skills/help.js
+++ skills/help.js
@@ -4,16 +4,16 @@
 module.exports = function (controller) {
 
     controller.hears([/^help$/], 'direct_message,direct_mention', function (bot, message) {
-        var text = "Here are my skills:";
-        text += "\n- " + bot.appendMention(message, "color") + ": ask to pick a random color";
-        text += "\n- " + bot.appendMention(message, "restricted") + ": let a user pick a color among a set of options";
-        text += "\n- " + bot.appendMention(message, "storage") + ": store picked color as a user preference";
-        text += "\n- " + bot.appendMention(message, "threads") + ": branch to another thread";
-        text += "\n- " + bot.appendMention(message, "variables") + ": enriched user-context among threads";
-        text += "\n\nI also understand:";
-        text += "\n- " + bot.appendMention(message, "about") + ": shows metadata about myself";
-        text += "\n- " + bot.appendMention(message, "help") + ": spreads the word about my skills";
-        text += "\n- " + bot.appendMention(message, "show [skill]") + ": display the code of the specified skill";
+        var text = "Aqui estão minhas habilidades:";
+        text += "\n- " + bot.appendMention(message, "color") + ": pergunta por uma cor";
+        text += "\n- " + bot.appendMention(message, "restricted") + ": deixa um usuário escolher uma cor dentro de um leque de opções";
+        text += "\n- " + bot.appendMention(message, "storage") + ": armazena a cor preferida do usuário";
+        text += "\n- " + bot.appendMention(message, "threads") + ": um grupo de threads";
+        text += "\n- " + bot.appendMention(message, "variables") + ": relação das threads dos usuários";
+        text += "\n\nI eu também entendo:";
+        text += "\n- " + bot.appendMention(message, "about") + ": sobre mim";
+        text += "\n- " + bot.appendMention(message, "help") + ": lista de habilidades";
+        text += "\n- " + bot.appendMention(message, "show [skill]") + ": mostra o código de uma habilidade específica";
         bot.reply(message, text);
     });
 }
